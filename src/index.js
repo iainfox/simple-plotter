e3725d11ef1d7b7df1651998e5edf4ee71a5f82d
iainfox <furghadmanar@gmail.com>
2026-05-01 21:43:57 -0500

    when user imports json, convert it and fill the template

---
+++ src/index.js
@@ -0,0 +1,56 @@
+const file_input = document.getElementById("file-input");
+if (!file_input) { throw new Error("No file input found")};
+
+const data_template = document.getElementById("data-template");
+if (!data_template) { throw new Error("No template found")};
+
+file_input.addEventListener("cancel", () => {
+	console.log("Cancelled.");
+});
+file_input.addEventListener("change", () => {
+	if (file_input.files.length !== 1) return
+
+	const file = file_input.files[0];
+
+	const reader = new FileReader();
+	reader.onload = () => {
+		fillTemplate(JSON.parse(reader.result))
+	};
+	reader.onerror = () => {
+		console.log("Error reading the file. Please try again.", "error");
+	};
+	reader.readAsText(file);
+});
+
+function fillTemplate(file) {
+	document.body.replaceChildren()
+
+	const clone = data_template.content.cloneNode(true);
+	document.body.appendChild(clone);
+
+	const heading_elem = document.getElementById("heading")
+	
+	const subheading_elem = document.getElementById("subheading")
+
+	const heading = file["heading"];
+	const subheading = file["subheading"];
+	
+	heading_elem.textContent = heading;
+	subheading_elem.textContent = subheading;
+
+	const data = file["data"];
+	const parsed_data = [];
+
+	for (const trace of Object.values(data)) {
+		let final_trace = {};
+
+		final_trace["x"] = trace["x-axis"]["data"];
+		final_trace["y"] = trace["y-axis"]["data"];
+		final_trace["type"] = "scatter";
+
+		parsed_data.push(final_trace);
+	}
+
+	Plotly.newPlot('plot-div', parsed_data);
+}
+
